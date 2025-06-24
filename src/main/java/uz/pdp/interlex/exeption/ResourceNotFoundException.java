40f638bf99d9f3ac0f95059a3b2e98e25be20be5
Abdugani Lapasov <180403589+Mavlon03@users.noreply.github.com>
2025-06-24 18:08:37 +0500

    Revert "First Commit"
    This reverts commit 8de53915fd75058861afe222d3efe02f5827bc8f.

--- src/main/java/uz/pdp/interlex/exeption/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package uz.pdp.interlex.exeption;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
